86ecd35165b496cba65102aa8f21e8ae892ce479
Jonas Hoffmann <s204071@student.dtu.dk>
2023-02-12 23:58:17 +0100

    add hydra

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,26 +1,33 @@
+import os
+
+import hydra
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.plugins.environments import LSFEnvironment
+from omegaconf import OmegaConf
 
 from src.data.datamodules.imagenet_datamodule import ImageNetDataModule
 from src.models.models.imagenet import ImageNet
 
 
-def main():
-    imagenet_module = ImageNet()
-    datamodule = ImageNetDataModule(batch_size=128, num_workers=8)
+@hydra.main(version_base=None, config_path=os.path.join(os.getcwd(), "config/imagenet"), config_name="default_config.yaml")
+def train(config):
+    print(f"configuration: \n {OmegaConf.to_yaml(config.training)}")
 
-    torch.set_float32_matmul_precision("medium")
+    hparams = config.training
+    pl.seed_everything(hparams["seed"])
+
+    imagenet_module = ImageNet()
+    datamodule = ImageNetDataModule(batch_size=hparams["batch_size"], num_workers=hparams["num_workers"])
 
     trainer = pl.Trainer(
-        accelerator="auto",
-        devices=2,
-        max_epochs=1,
-        strategy="ddp",
-        num_nodes=2
+        accelerator=hparams["accelerator"],
+        devices=hparams["devices"],
+        max_epochs=hparams["n_epochs"],
+        strategy=hparams["strategy"],
+        num_nodes=hparams["num_nodes"]
     )
     trainer.fit(imagenet_module, datamodule=datamodule)
 
 
 if __name__ == "__main__":
-    main()
+    train()
