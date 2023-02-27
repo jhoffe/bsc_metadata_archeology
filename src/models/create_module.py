6b0d7853c449a32023b6de72d3cff95ac5fb8d52
Jonas Hoffmann <s204071@student.dtu.dk>
2023-02-27 10:32:06 +0100

    add creators

---
+++ src/models/create_module.py
@@ -0,0 +1,17 @@
+from omegaconf import DictConfig
+
+from src.models.models import ImageNetResNet50
+
+
+def create_module(params: DictConfig):
+    MODULES = {"imagenet-resnet50": ImageNetResNet50}
+
+    model_params = params.model
+    module_name = model_params["name"]
+
+    if module_name not in MODULES.keys():
+        raise ValueError(f"model '{module_name}' not in modules")
+
+    return MODULES[module_name](
+        **{key: value for key, value in model_params.items() if key != "name"}
+    )
