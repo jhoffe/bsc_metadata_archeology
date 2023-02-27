3561b55f3b7093ea7031856d98d198ff40baf1c5
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-02-27 11:20:59 +0100

    ez

--- test.py
+++
@@ -1,9 +0,0 @@
-import torch 
-#import __main__
-
-#setattr(__main__, "CustomCIFAR10", CustomCIFAR10)
-
-
-    #from src.data.c_scores import CustomCIFAR10, CustomCIFAR100
-data = torch.load("data/processed/cifar10/train.pt")
-
