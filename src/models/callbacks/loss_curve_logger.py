6cc5a32865f45cc7500820cdbe589881a5e11d3f
Jonas Hoffmann <jonas@triweb.dk>
2023-03-06 12:47:07 +0100

    remove print

--- src/models/callbacks/loss_curve_logger.py
+++ src/models/callbacks/loss_curve_logger.py
@@ -36,10 +36,8 @@ class LossCurveLogger(Callback):
     ) -> None:
         batch_ids = []
         indices = torch.cat([indices for _, _, indices in self.loss_curves], 0).to(
-            torch.int32
+            pl_module.dtype
         )
-
-        print(indices.dtype)
 
         for batch_idx, _, fns in self.loss_curves:
             batch_ids.extend([batch_idx] * len(fns))
@@ -53,7 +51,9 @@ class LossCurveLogger(Callback):
                     torch.zeros(losses.shape, device=pl_module.device)
                 ] * trainer.num_devices
                 device_indices = [
-                    torch.zeros(losses.shape, dtype=torch.int32)
+                    torch.zeros(
+                        losses.shape, device=pl_module.device, dtype=pl_module.dtype
+                    )
                 ] * trainer.num_devices
 
                 torch.distributed.gather_object(batch_ids, device_batch_ids)
