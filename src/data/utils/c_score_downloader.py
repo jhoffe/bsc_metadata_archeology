0b5cd9dad822ec69a92cfb9a337e667c0f021399
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-03-11 14:57:29 +0100

    download fix

--- src/data/utils/c_score_downloader.py
+++ src/data/utils/c_score_downloader.py
@@ -1,4 +1,6 @@
 import requests
+import pathlib
+import numpy as np
 
 
 def c_score_downloader():
@@ -13,6 +15,18 @@ def c_score_downloader():
         r = requests.get(URL)
         open(f"data/external/{file}", "wb").write(r.content)
 
+def mem_score_downloader():
+    """Takes a list of files and downloads these into the data/external folder."""
+    cscores = [
+        "cifar100_infl_matrix.npz",
+        "imagenet_index.npz"
+    ]
+    for file in cscores:
+        URL = f"https://pluskid.github.io/influence-memorization/data/{file}"
+        r = requests.get(URL)
+        open(f"data/external/{file}", "wb").write(r.content)
 
 if __name__ == "__main__":
     c_score_downloader()
+    mem_score_downloader()
+
