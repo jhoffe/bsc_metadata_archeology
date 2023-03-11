c042b7b46ce4105d39d3975220549ad3367841ef
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-03-11 14:48:06 +0100

    imagenet mem_scores

--- src/data/imagenet_c_scores.py
+++ src/data/imagenet_c_scores.py
@@ -7,17 +7,19 @@ from src.data.utils.c_score_downloader import c_score_downloader
 
 def imagenet_c_scores():
     """Get imagenet-c scores."""
-    file = pathlib.Path("data/external/imagenet-cscores-with-filename.npz")
+    #file = pathlib.Path("data/external/imagenet-cscores-with-filename.npz")
 
-    if not file.exists():
-        c_score_downloader()
+    #if not file.exists():
+    #    c_score_downloader()
 
-    cscores = np.load(file, allow_pickle=True)
+    file = pathlib.Path("data/external/imagenet_index.npz")
+
+    scores = np.load(file, allow_pickle=True)
 
     return {
-        "labels": cscores["labels"],
-        "scores": 1 - cscores["scores"],
-        "filenames": [f.decode("utf-8") for f in cscores["filenames"]],
+        "labels": scores["tr_labels"],
+        "scores": 1 - scores["tr_mem"],
+        "filenames": [f.decode("utf-8") for f in scores["tr_filenames"]],
     }
 
 
