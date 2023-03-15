ddf2f71041bd2c828a0997b267b09e9586af2337
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-03-15 20:19:03 +0100

    idk

--- src/data/cifar_probes.py
+++ src/data/cifar_probes.py
@@ -1,7 +1,20 @@
 import torch
+import matplotlib.pyplot as plt
+from src.data.idx_to_label_names import get_idx_to_label_names_cifar100
 
-cifar10 = torch.load("data/processed/cifar10/train_probe_suite.pt")
+#cifar10 = torch.load("data/processed/cifar10/train_probe_suite.pt")
 cifar100 = torch.load("data/processed/cifar100/train_probe_suite.pt")
 
-print(cifar10.random_outputs[0])
-print(cifar100.random_outputs[0])
+idx_to_label = get_idx_to_label_names_cifar100()
+
+#print(cifar10.random_outputs[0])
+random_outputs = cifar100.corrupted
+print(random_outputs[0][0])
+
+testid = 478
+# Plot the image
+plt.imshow(random_outputs[testid][0][0].permute(1,2,0))
+# Title
+label = idx_to_label[random_outputs[testid][0][1]]
+plt.title(f"Random Output w. label {label}")
+plt.show()
--- src/data/idx_to_label_names.py
+++ src/data/idx_to_label_names.py
@@ -10,3 +10,17 @@ def get_idx_to_label_names() -> dict[int, str]:
     idx2labelname = eval(lines)
 
     return idx2labelname
+
+def get_idx_to_label_names_cifar100() -> dict[int, str]:
+    classes = ["beaver", "dolphin", "otter", "seal", "whale", "aquarium fish", "flatfish", "ray", "shark", "trout",
+                   "orchids", "poppies", "roses", "sunflowers", "tulips", "bottles", "bowls", "cans", "cups", "plates",
+                   "apples", "mushrooms", "oranges", "pears", "sweet peppers", "clock", "computer keyboard", "lamp",
+                   "telephone", "television", "bed", "chair", "couch", "table", "wardrobe", "bee", "beetle", "butterfly",
+                   "caterpillar", "cockroach", "bear", "leopard", "lion", "tiger", "wolf", "bridge", "castle", "house",
+                   "road", "skyscraper", "cloud", "forest", "mountain", "plain", "sea", "camel", "cattle", "chimpanzee",
+                   "elephant", "kangaroo", "fox", "porcupine", "possum", "raccoon", "skunk", "crab", "lobster", "snail",
+                   "spider", "worm", "baby", "boy", "girl", "man", "woman", "crocodile", "dinosaur", "lizard", "snake",
+                   "turtle", "hamster", "mouse", "rabbit", "shrew", "squirrel", "maple", "oak", "palm", "pine", "willow",
+                   "bicycle", "bus", "motorcycle", "pickup truck", "train", "lawn-mower", "rocket", "streetcar", "tank", "tractor"]
+    label2name = {k: v for k, v in enumerate(classes)}
+    return label2name
