dab7d859912c244da4e733c56ac1a6b1bf477bd3
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-03-17 12:09:34 +0100

    test

--- src/data/make_dataset_cifar10.py
+++ src/data/make_dataset_cifar10.py
@@ -2,20 +2,18 @@ import logging
 from pathlib import Path
 
 import click
-import pytorch_lightning as pl
 from dotenv import find_dotenv, load_dotenv
 
 from src.data.c_scores import c_scores_dataset
 from src.data.download import download_dataset
 from src.data.make_probe_suites import make_probe_suites
-from src.data.transform import dataset_transform
 
 
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    #pl.seed_everything(123)
+    # pl.seed_everything(123)
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -24,10 +22,10 @@ def main(input_filepath, output_filepath):
 
     logger.info("Downloading the CIFAR10 and CIFAR100 datasets.")
     download_dataset(input_filepath, "cifar10")
-    
+
     logger.info("Transforming the CIFAR10 dataset")
     c_scores_dataset("cifar10", "data/raw", "data/processed")
-    
+
     # Generating probe suites
     logger.info("Generating probe suites for CIFAR10")
     make_probe_suites("data/processed", "data/processed", "cifar10", label_count=10)
