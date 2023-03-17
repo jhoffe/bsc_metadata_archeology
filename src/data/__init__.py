9e4459748253500f38e047b77ac1788463e118db
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-03-17 15:40:55 +0100

    cleanup

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,5 @@
 from .c_scores import CustomCIFAR10, CustomCIFAR100
-from .idx_to_label_names import get_idx_to_label_names
+from .utils.idx_to_label_names import get_idx_to_label_names
 from .loss_dataset import LossDataset
 
 __all__ = ["CustomCIFAR10", "CustomCIFAR100", "LossDataset", "get_idx_to_label_names"]
