01d6c62d69a93e50367a3f7f819506990daee545
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-03-17 13:14:01 +0100

    remove print

--- src/data/make_probe_suites.py
+++ src/data/make_probe_suites.py
@@ -174,7 +174,6 @@ class ProbeSuiteGenerator(Dataset):
 
     def __getitem__(self, index):
         if index in self.used_indices:
-            print("Warning: using probe data for training")
             return self.combined[self.dataset_indices_to_probe_indices[index]]
         return self.dataset[index], index
 
