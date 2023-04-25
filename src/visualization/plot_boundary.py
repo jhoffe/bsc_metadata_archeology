7a279336bdd4c47ea7dc81ae6de3a567e22055b6
Jonas Hoffmann <s204071@student.dtu.dk>
2023-04-25 22:55:12 +0200

    fix tsne

--- src/visualization/plot_boundary.py
+++ src/visualization/plot_boundary.py
@@ -1,23 +1,10 @@
 import click
 import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.base import BaseEstimator, is_classifier
-from sklearn.inspection import DecisionBoundaryDisplay
+from lightning import seed_everything
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
 
 from src.data.loss_dataset import LossDataset
-
-
-def train_metadata_model(
-    classifier: BaseEstimator, X: np.array, y: np.array
-) -> BaseEstimator:
-    assert is_classifier(classifier)
-
-    classifier.fit(X, y)
-
-    return classifier
 
 
 @click.command()
@@ -27,7 +14,9 @@ def train_metadata_model(
 @click.argument(
     "loss_dataset_path", type=click.Path(exists=True, dir_okay=True, file_okay=False)
 )
-def main(probe_suite_path, loss_dataset_path):
+@click.option("--seed", type=int, default=42)
+def main(probe_suite_path, loss_dataset_path, seed):
+    seed_everything(seed)
     loss_dataset = LossDataset(loss_dataset_path)
 
     print("Loading loss dataset...")
@@ -36,37 +25,30 @@ def main(probe_suite_path, loss_dataset_path):
     loss_dataset.load_probe_suite(probe_suite_path)
 
     print("Creating train matrix...")
-    X_train, y_train = loss_dataset.to_sklearn_train_matrix()
+    X, y = loss_dataset.to_sklearn_train_matrix()
 
     print("Standardizing...")
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
+    X = scaler.fit_transform(X)
 
-    print("PCA...")
-    pca = TSNE(n_components=2, n_jobs=-1)
-    X_train = pca.fit_transform(X_train)
+    print("TSNE...")
+    tsne = TSNE(
+        n_components=3, n_jobs=-1, random_state=seed, perplexity=100, init="pca"
+    )
+    X = tsne.fit_transform(X)
 
-    # print("Explained variance ratio: ", pca.explained_variance_ratio_)
-
-    # classifier = XGBClassifier(n_estimators=100)
-    classifier = SVC()
-    print("Training model...")
-    classifier = train_metadata_model(classifier, X_train, y_train)
-
-    print("Plotting decision boundary...")
-    disp = DecisionBoundaryDisplay.from_estimator(classifier, X_train, alpha=0.5)
+    fig, ax = plt.subplots(figsize=(10, 10))
 
     suite_names = set(loss_dataset.probe_suite.index_to_suite.values())
-    print(suite_names)
     idx_to_label = {i: label for i, label in enumerate(suite_names)}
 
     for i, label in idx_to_label.items():
-        disp.ax_.scatter(
-            X_train[y_train == i, 0], X_train[y_train == i, 1], label=label
+        ax.scatter(
+            X[y == i, 0], X[y == i, 1], label=label.replace("_", " ").capitalize()
         )
-    plt.legend()
 
-    plt.show()
+    plt.legend()
+    plt.savefig("reports/figures/tsne_decision_boundary_with_scaling.png")
 
 
 if __name__ == "__main__":
