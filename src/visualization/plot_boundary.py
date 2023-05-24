e60d07582deb86fbbf3db2d9dc7b69cd5a2e284d
Jonas Hoffmann <s204071@student.dtu.dk>
2023-05-24 10:23:24 +0200

    add job for vit plots

--- src/visualization/plot_boundary.py
+++ src/visualization/plot_boundary.py
@@ -1,23 +1,11 @@
+import logging
+
 import click
 import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.base import BaseEstimator, is_classifier
-from sklearn.decomposition import PCA
-from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
 
 from src.data.loss_dataset import LossDataset
-
-
-def train_metadata_model(
-    classifier: BaseEstimator, X: np.array, y: np.array
-) -> BaseEstimator:
-    assert is_classifier(classifier)
-
-    classifier.fit(X, y)
-
-    return classifier
 
 
 @click.command()
@@ -28,46 +16,44 @@ def train_metadata_model(
     "probe_suite_path", type=click.Path(exists=True, dir_okay=False, file_okay=True)
 )
 def main(loss_dataset_path, probe_suite_path):
+    logger = logging.getLogger(__name__)
+
     loss_dataset = LossDataset(loss_dataset_path)
 
-    print("Loading loss dataset...")
+    logger.info("Loading loss dataset...")
     loss_dataset.load()
-    print("Loading probe suite...")
+    logger.info("Loading probe suite...")
     loss_dataset.load_probe_suite(probe_suite_path)
 
-    print("Creating train matrix...")
+    logger.info("Creating train matrix...")
     X_train, y_train = loss_dataset.to_sklearn_train_matrix()
 
-    print("Standardizing...")
+    logger.info("Standardizing...")
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
 
-    print("PCA...")
-    pca = PCA(n_components=2)
-    X_train = pca.fit_transform(X_train)
-
-    print("Explained variance ratio: ", pca.explained_variance_ratio_)
-
-    # classifier = XGBClassifier(n_estimators=100)
-    classifier = SVC()
-    print("Training model...")
-    classifier = train_metadata_model(classifier, X_train, y_train)
-
-    print("Plotting decision boundary...")
-    disp = DecisionBoundaryDisplay.from_estimator(classifier, X_train, alpha=0.5)
+    logger.info("TSNE...")
+    tsne = TSNE(n_components=2, perplexity=50, n_iter=2500, random_state=42, n_jobs=-1)
+    X_train = tsne.fit_transform(X_train)
 
     suite_names = set(loss_dataset.probe_suite.index_to_suite.values())
-    print(suite_names)
+    logger.info(suite_names)
     idx_to_label = {i: label for i, label in enumerate(suite_names)}
 
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plt.tight_layout()
+
     for i, label in idx_to_label.items():
-        disp.ax_.scatter(
+        ax.scatter(
             X_train[y_train == i, 0], X_train[y_train == i, 1], label=label
         )
     plt.legend()
 
-    plt.show()
+    fig.savefig("reports/figures/tsne/tsne.png")
 
 
 if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
     main()
